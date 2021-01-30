8b6975d8c40c3910bac27b39a90ecfa7eaea9ee8
Infra-Blue <rudro1152@gmail.com>
2021-01-30 16:08:17 +0600

    constructed modular expnoentiation function

--- rsa.cpp
+++ rsa.cpp
@@ -1,4 +1,10 @@
-int modular_exp(int base, int pow, int mod) 
+unsigned long modular_exp(const int &base, int pow, const int &mod) 
 {
-    
+    static const int base_mod = base % mod;
+    if (pow == 1)
+        return base_mod;
+    unsigned long half_compute, res;
+    half_compute = modular_exp(base, pow >> 1, mod);
+    res = (half_compute * half_compute) % mod; 
+    return pow & 1 ? (base_mod * res) % mod : res % mod;
 }
