83fc5b2cd05b2ce16115e21f6e80fd244bed36bc
Infra-Blue <rudro1152@gmail.com>
2021-01-31 08:48:53 +0600

    constructed modular inverse function

--- rsa.cpp
+++ rsa.cpp
@@ -1,3 +1,21 @@
+#include <utility>
+
+std::pair<int, int> extended_eucledian(int e, int totient)
+{
+    if (totient == 1)
+        return {0, 1};
+    auto res(extended_eucledian(totient, e % totient));
+    return {res.second, res.first - (e / totient) * res.second};
+}
+
+
+int modular_inverse(int e, int totient)
+{
+    int res = extended_eucledian(e, totient).first;
+    return res < 0 ? totient + res : res;
+}
+
+
 unsigned long modular_exp(const int &base, int pow, const int &mod) 
 {
     static const int base_mod = base % mod;
@@ -8,3 +26,5 @@ unsigned long modular_exp(const int &base, int pow, const int &mod)
     res = (half_compute * half_compute) % mod; 
     return pow & 1 ? (base_mod * res) % mod : res % mod;
 }
+
+
